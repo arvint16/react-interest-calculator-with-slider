9336b878c77a1fb85887c9f95632568d2b643d48
Arvint <arvint.balamurali@gmail.com>
2020-11-26 20:25:34 +0530

    #comment completed-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import taskImage from './taskImage.PNG'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const footerContent = <>'*Estimated earnings are based on -1.16% per annum before tax and any potential losses.
+As with most forms of investment, peer-to-peer leading carris a degree of risk to your capital; in this case, if borrowers were unable to 
+repay their loans.'</>
+
+const perYearValue = (5000/86).toFixed(2) , feesValue = (perYearValue/10).toFixed(2)
+
+export default class App extends Component {
+
+  constructor(props){
+  super(props)
+    this.state = {
+      amountValue: 5000,
+      perYear: perYearValue,
+      fees: feesValue,
+      inputField:false
+    };
+  }
+
+  yearCalc =(value) => {
+    this.setState({perYear:(value/86).toFixed(2)},() => this.feesCalc(this.state.perYear)) 
+   }
+
+   feesCalc = (value) => {
+     this.setState({fees: (value/10).toFixed(2)})
+   }
+
+  handleAmountChange = (event,key) => {
+    if(key === 'input' && event.target.value <= 260000){
+      this.setState({inputField:true})
+    }
+    else this.setState({inputField:false})
+
+  if(event.target.value <= 260000){
+    this.setState({amountValue: event.target.value}, () => 
+    this.yearCalc( event.target.value)
+  )
+}
+  };
+
+  render() {
+
+    const {
+      amountValue,
+      perYear,
+      fees,
+      inputField
+    } = this.state
+
+    return (
+      <div className="App">
+      <div className='container'>
+        <div>
+          <img src={taskImage} alt='image' width='100%' height='50%'/>
+          </div>
+      <div className='content'>
+      <div>
+    <div style={{fontSize:'24px'}} onChange={(e)=>this.handleAmountChange(e,'input')}> 
+    With 
+    <div className='inputFieldContainer'>
+    <span style={{borderBottom:'1px solid #03A9F4'}}>
+    $ {!inputField ?
+     <input type='number' className='inputField' value={amountValue}/> :
+    <input type='number' className='inputField' />
+   } 
+    </span>
+    </div>   
+    your estimated earnings could be<span className='earningContent'>$- {perYear}</span>* per year with fees of just
+    <span className='feesContent'>$- {fees}</span>per year 
     </div>
-  );
+
+      <div className='footerContent'>{footerContent} </div>
+    </div>
+    <input className="slider" type="range" min="0" max="260000"  width='100%' value={amountValue} onChange={(e)=>this.handleAmountChange(e,'slider')} />
+    </div>
+    </div>
+      </div>
+    );
+  }
 }
 
-export default App;
